6f40b16bdaa3c1c4940c5fe9911318c4c73357cf
iqbalO2018 <oiqbal@sjnma.org>
2018-02-05 10:20:09 -0600

    pypractice 1

---
+++ variables.py
@@ -0,0 +1,15 @@
+print ('''hello world!
+''')
+
+first_name = raw_input("what is your name? ")
+
+last_name = raw_input("what is your last name? ")
+
+full_name = first_name + last_name
+
+print ("cool name, " +full_name)
+
+favorite_color = raw_input("What is your favorite color? ")
+
+print (favorite_color + "That's my favorite too!")
+
