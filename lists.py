99fc144089efb5faeae9e32ec707d060e0dd6a7c
iqbalO2018 <oiqbal@sjnma.org>
2018-02-26 11:20:18 -0600

    pypractice

---
+++ lists.py
@@ -0,0 +1,23 @@
+socks = "socks"
+jeans = "jeans"
+laptop = "laptop"
+toiletries = "toiletries"
+
+luggage = []
+item1 = raw_input("what you like to pack?")
+item2 = raw_input("what you like to eat on a trip?")
+item3 = raw_input("favorite songs on a trip?")
+item4 = raw_input("place to travel?")
+luggage.append(item1)
+luggage.append(item2)
+luggage.append(item3)
+luggage.append(item4)
+print(luggage)
+
+grades = [
+96.0
+100.0
+75.5
+89.5
+]
+print(grades,sum(grades)/len(grades))
